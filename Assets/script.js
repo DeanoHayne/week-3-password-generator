e84017bbb6a41209335536ca8c8291c1feadbecf
Dean Hay <deanhay16@gmail.com>
2022-03-10 23:50:26 +1100

    added possible characters for password

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,32 @@
+var characterLength = 8;
+var choiceArr = [];
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '*', '+'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['1', '2', '3', '3', '4', '5', '6', '6', '7', '8', '9', '0'];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    console.log("You clicked the button");
 
-    passwordText.value = password;
+    // 1. Prompt the user for the password 
+    // a. Password length 8 to 128
+    // b. Lowercase, uppecase, numbers, special characters
+    // 2. Vaildate the input
+    // 3.Generate password based on criteria
+    // 4. Generate password to the page
+
+
+
+    return "Generated password will return here";
+
+}
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
 
 }
 
